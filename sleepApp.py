69bbb53dfd5f846d7ee996693b12c8314079b688
JC <jcquah2@gmail.com>
2023-06-20 01:35:43 +0800

    update

--- sleepApp.py
+++ sleepApp.py
@@ -13,8 +13,6 @@ model2 = joblib.load(r"best_model_sleep_apnea.pkl")
 model3 = joblib.load(r"best_model_insomnia.pkl")
 
 with st.sidebar:
-    image2 = Image.open("Logo.png")
-    st.image(image2, use_column_width=True)
     selected = option_menu(
         "Sweet Dream 💤 : Main Menu",
         ["🥱 Introduction", "😴 Sleep Efficiency Predictor", "😪 Sleep Disorder Predictor"],
